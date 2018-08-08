f52930da8a908a112f844a120a495006432dbcdc
davidngatia <machngatia@gmail.com>
2018-08-08 17:47:36 +0300

    add calculator to github

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1,number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter another number:"));
+var number2 = parseInt(prompt("Enter another number"));
+alert(multiply(number1,number2));
